dc1817c5e833c59ca1346b61537c480945a1232f
BogdanLazebnika <128160081+BogdanLazebnika@users.noreply.github.com>
2024-01-01 15:32:01 +0200

    Add files via upload

---
+++ Fannia store/asets/js/linc.js
@@ -0,0 +1,37 @@
+// Зміна тексту і посилання в верхній навігації
+    // 1
+        var element = document.getElementById('left__top-menu-1');
+        // element.innerHTML = 'Магазин';
+        element.href = 'shop.html';
+    // 2
+        var element = document.getElementById('left__top-menu-2');
+        // element.innerHTML = 'Допомога';
+        element.href = '';
+    // 2-1 
+        var element = document.getElementById('left__top-menu-2/1');
+        // element.innerHTML = '';
+        element.href = '';
+    // 2-2
+        var element = document.getElementById('left__top-menu-2/1');
+        // element.innerHTML = '';
+        element.href = '';
+    // 2-3 
+        var element = document.getElementById('left__top-menu-2/1');
+        // element.innerHTML = '';
+        element.href = '';
+    // 2-4 
+        var element = document.getElementById('left__top-menu-2/1');
+        // element.innerHTML = '';
+        element.href = '';
+    // 3
+        var element = document.getElementById('left__top-menu-3');
+        // element.innerHTML = '';
+        element.href = '';
+    // 4
+        var element = document.getElementById('left__top-menu-4');
+        // element.innerHTML = '';
+        element.href = '';
+    // 5
+        var element = document.getElementById('');
+        element.innerHTML = '';
+        element.href = '';
